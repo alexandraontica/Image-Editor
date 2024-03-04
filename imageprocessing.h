e891ba64ac20308f472badede14882fa00d38068
Alexandra Onțică <150956540+alexandraontica@users.noreply.github.com>
2024-03-05 00:12:14 +0200

    Add files via upload

--- imageprocessing.h
+++ imageprocessing.h
@@ -7,5 +7,7 @@ int ***crop(int ***image, int N, int M, int x, int y, int h, int w);
 int ***extend(int ***image, int N, int M, int rows, int cols, int new_R, int new_G, int new_B);
 int ***paste(int ***image_dst, int N_dst, int M_dst, int *** image_src, int N_src, int M_src, int x, int y);
 int ***apply_filter(int ***image, int N, int M, float **filter, int filter_size);
+int ***alloc_mat(int M, int N);
+void free_mat(int ***mat, int M, int N);
 
 #endif  // IMAGEPROCESSING_H_INCLUDED
